30520b5dc9bfd579e9e9ac6dd352f9d42115a29e
Ronny Schlidt <r.schlidt1@web.de>
2024-06-20 12:20:27 +0800

    fix vercel?

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,6 @@
 	import '$lib/index.scss';
 	import { onHydrated, theme } from '$lib/stores/theme';
 	import { onMount } from 'svelte';
-
 	// ? moved to +layout.server.ts : will be deleted when we make sure that everything is alright
 	// export const prerender = true;
 
